3736babe05c66c5c39d82d23195c0a1952a64891
João Alberto <hey@joaoalberto.dev>
2024-12-11 18:01:08 -0300

    fix: prevent user from save empty notes

--- src/features/editor/ui/editor-actions.tsx
+++ src/features/editor/ui/editor-actions.tsx
@@ -8,9 +8,11 @@ import { toast } from "sonner";
 function EditorActions() {
   const { editor } = useCurrentEditor();
   const [isPending, startTransition] = useTransition();
+  const disabled =
+    editor?.state.doc.textContent.trim() === "" || isPending;
 
   async function handleSave() {
-    if (!editor) return;
+    if (!editor || disabled) return;
 
     startTransition(async () => {
       try {
@@ -26,8 +28,8 @@ function EditorActions() {
     <div className="absolute bottom-2 right-2">
       <Button
         onClick={handleSave}
-        className="bg-white rounded-full flex items-center justify-center w-10 h-10 z-10"
-        disabled={isPending}
+        className="bg-white rounded-full flex items-center justify-center w-10 h-10 z-10 disabled:opacity-50 disabled:cursor-not-allowed"
+        disabled={disabled}
       >
         {isPending ? (
           <Loader2 className="w-4 h-4 animate-spin" />
