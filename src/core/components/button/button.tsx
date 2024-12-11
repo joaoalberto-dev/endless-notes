672441328fc3ba01fb551ef7d2ea9954f74f53d1
João Alberto <hey@joaoalberto.dev>
2024-12-11 17:39:00 -0300

    feat: new generic button

---
+++ src/core/components/button/button.tsx
@@ -0,0 +1,43 @@
+import { cn } from "@/core/utils/cn";
+import { ButtonHTMLAttributes } from "react";
+
+const baseClass =
+  "rounded-md cursor-pointer px-2 py-1 border border-gray-300 data-[active=true]:bg-gray-100";
+
+const disabledClass = "cursor-not-allowed";
+
+const variants = {
+  default: "",
+  primary: "bg-blue-500 text-white",
+};
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: keyof typeof variants;
+  active?: boolean;
+};
+
+function Button({
+  children,
+  className,
+  variant = "default",
+  active,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      type="button"
+      data-active={active}
+      className={cn(
+        baseClass,
+        props.disabled && disabledClass,
+        variants[variant],
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
+
+export { Button };
