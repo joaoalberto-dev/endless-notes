813756b5a9470836030b916297813020d4456511
João Alberto <hey@joaoalberto.dev>
2024-12-11 17:40:51 -0300

    feat: save note action

---
+++ src/features/editor/data/save-note.ts
@@ -0,0 +1,11 @@
+import { db } from "@/core/services/firebase";
+import { addDoc } from "firebase/firestore";
+import { collection } from "firebase/firestore";
+
+async function saveNote(note: string) {
+  const notesCollection = collection(db, "notes");
+
+  return await addDoc(notesCollection, { note });
+}
+
+export { saveNote };
