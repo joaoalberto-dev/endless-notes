431c6eea3ca7c289df992c60495865ced111bd0c
João Alberto <hey@joaoalberto.dev>
2024-12-11 19:09:52 -0300

    chore: sort notes

--- src/features/notes/data/get-notes.ts
+++ src/features/notes/data/get-notes.ts
@@ -1,18 +1,19 @@
-import { collection } from "firebase/firestore";
-
+import { collection, orderBy, query } from "firebase/firestore";
 import { db } from "@/core/services/firebase";
 import { getDocs } from "firebase/firestore";
 import { Note } from "../types";
 
 async function getNotes(): Promise<Note[]> {
   const notesCollection = collection(db, "notes");
-
-  const notes = await getDocs(notesCollection);
+  const q = query(notesCollection, orderBy("createdAt", "asc"))
+  const notes = await getDocs(q);
 
   return notes.docs.map((doc) => {
     return {
       id: doc.id,
       note: doc.data().note,
+      createdAt: doc.data().createdAt,
+      updatedAt: doc.data().updatedAt,
     };
   });
 }
