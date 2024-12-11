ffefada2f2bbf7f89fd070917c6ad09d425ad742
João Alberto <hey@joaoalberto.dev>
2024-12-11 18:40:29 -0300

    chore: rendering react-query provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { QueryClientProvider, queryClient } from "./core/query";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
