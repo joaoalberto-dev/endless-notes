1aec2a5b516b3bfbabacb68526f90a672b6adce3
João Alberto <hey@joaoalberto.dev>
2024-12-11 17:45:38 -0300

    feat: rendering the editor

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import "./App.css";
 import { signOut } from "@/features/auth/data/auth";
 import { useUser } from "@/features/auth/hooks/use-user";
 import { SignInPage } from "./features/auth/ui/sign-in-page";
+import { Editor } from "./features/editor/ui/editor";
+import { Toaster } from "sonner";
 
 function App() {
   const [user, isPending] = useUser();
@@ -13,10 +15,13 @@ function App() {
   }
 
   return (
-    <>
-      <p>User {user.displayName} is logged in</p>
-      <button onClick={signOut}>Sign out</button>
-    </>
+    <div className="relative flex flex-col h-screen w-screen items-center justify-center">
+      <div className="fixed top-0 right-0 p-4 flex justify-between items-center">
+        <button onClick={signOut}>Sign out</button>
+      </div>
+      <Editor />
+      <Toaster />
+    </div>
   );
 }
 
