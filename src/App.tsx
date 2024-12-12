71d93b8259c92bf84337c797b481afd3d0b656d8
João Alberto <hey@joaoalberto.dev>
2024-12-11 23:16:29 -0300

    chore: refactor loading state and layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,24 @@ import "./App.css";
 import { useUser } from "@/features/auth/hooks/use-user";
 import { SignInPage } from "./features/auth/ui/sign-in-page";
 import { Editor } from "./features/editor/ui/editor";
-import { Toaster } from "sonner";
 import { NotesList } from "./features/notes/ui/notes-list";
+import LoaderPage from "./core/components/loader/loader-page";
+import { PageLayout } from "./core/components/layout/page-layout";
 
 function App() {
   const [user, isPending] = useUser();
-  const signedIn = user !== null && !isPending;
 
-  if (!signedIn) {
-    return <SignInPage />;
-  }
+  if (isPending) return <LoaderPage />;
+
+  if (!user) return <SignInPage />;
 
   return (
-    <div className="relative flex flex-col h-screen w-screen overflow-x-hidden pb-32 items-start sm:items-center justify-start">
+    <PageLayout>
       <div className="sm:pt-[calc(50vh-150px)] w-full flex justify-center">
         <Editor />
       </div>
       <NotesList />
-      <Toaster />
-    </div>
+    </PageLayout>
   );
 }
 
